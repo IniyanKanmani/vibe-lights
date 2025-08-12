e6380d2c74c9a9c55297f5cb27d561c283bccebc
IniyanKanmani <kiniyan002@gmail.com>
2025-08-12 14:36:40 +0530

    feat: modified rest api client to control all light at once

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from time import sleep
 
 from dotenv import load_dotenv
@@ -7,13 +8,17 @@ from home_assistant_rest_api import HomeAssistantRestAPI
 
 def main():
     load_dotenv()
+    backend = os.getenv("BACKEND")
 
-    ha_rest_api = HomeAssistantRestAPI()
-    ha_rest_api.fetch_all_lights()
+    if backend == "restapi":
+        ha_rest_api = HomeAssistantRestAPI()
+        ha_rest_api.fetch_all_lights()
 
-    ha_rest_api.control_lights([255, 255, 255])
-    sleep(5)
-    ha_rest_api.turn_lights_off()
+        for _ in range(10):
+            ha_rest_api.control_lights([255, 255, 255])
+            sleep(0.5)
+            ha_rest_api.turn_lights_off()
+            sleep(0.5)
 
 
 if __name__ == "__main__":
