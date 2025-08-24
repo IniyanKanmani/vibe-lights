d1cf2c7293d385fd5d152d9315de25032fd3c295
IniyanKanmani <kiniyan002@gmail.com>
2025-08-24 23:00:39 +0530

    feat: tuya local process complete setup

--- local_tuya_process.py
+++ local_tuya_process.py
@@ -4,10 +4,10 @@ import queue
 import threading
 from json import dumps, loads
 from multiprocessing.connection import Connection
+from time import sleep
 from typing import List
 
-import tinytuya
-from tinytuya import wizard
+from tinytuya import BulbDevice, scanner, wizard
 
 
 class LocalTuyaProcess(multiprocessing.Process):
@@ -16,18 +16,14 @@ class LocalTuyaProcess(multiprocessing.Process):
     ) -> None:
         super().__init__()
 
+        scanner.SCANTIME = 30
+
         self.__process_connection = process_connection
         self.__process_queue = process_queue
 
         self.__connection_status = False
 
     def __initialize(self) -> None:
-        scan = tinytuya.deviceScan(byID=True)
-        print(scan)
-
-        if not scan:
-            return
-
         config = {
             "apiKey": os.getenv("TUYA_API_KEY"),
             "apiSecret": os.getenv("TUYA_API_SECRET"),
@@ -40,36 +36,36 @@ class LocalTuyaProcess(multiprocessing.Process):
 
         wizard.wizard(
             assume_yes=True,
-            skip_poll=True,
+            skip_poll=False,
         )
-
-        os.unlink("tuya-raw.json")
 
         with open("devices.json", "r") as f:
             devices = loads(f.read())
 
             self.__devices = []
             for device in devices:
-                try:
-                    if device["category"] == "dj":
-                        d = {
-                            "id": device["id"],
-                            "name": device["name"],
-                            "ip_address": scan[device["id"]]["ip"],
-                            "local_key": device["key"],
-                            "version": scan[device["id"]]["version"],
-                        }
+                if device["category"] == "dj":
+                    data = {
+                        "id": device["id"],
+                        "name": device["name"],
+                        "ip_address": device["ip"],
+                        "local_key": device["key"],
+                        "version": device["version"],
+                    }
 
-                        self.__devices.append(d)
-                except KeyError:
-                    print(device)
+                    self.__devices.append(data)
+
+        os.unlink("snapshot.json")
+        os.unlink("tuya-raw.json")
+        os.unlink("devices.json")
+        os.unlink("tinytuya.json")
 
     def __connect(self) -> None:
-        self.__lights: List[tinytuya.BulbDevice] = []
+        self.__lights = []
         self.__initial_light_states = {}
 
         for device in self.__devices:
-            light = tinytuya.BulbDevice(
+            light = BulbDevice(
                 dev_id=device["id"],
                 address=device["ip_address"],
                 local_key=device["local_key"],
@@ -78,31 +74,49 @@ class LocalTuyaProcess(multiprocessing.Process):
             )
             self.__lights.append(light)
 
-            self.__initial_light_states[device["id"]] = light.status()["dps"]
+            status = light.status()
+            self.__initial_light_states[device["id"]] = status["dps"]
 
-        print(self.__initial_light_states)
+            light.set_mode("colour", nowait=True)
 
         self.__connection_status = True
 
     def __send_light_state(self, brightness: int, rgb_color: List[int]) -> None:
-        for light in self.__lights:
-            light.set_brightness(int((brightness / 255) * 1000), nowait=True)
-            # light.set_brightness(brightness, nowait=True)
-            light.set_colour(*rgb_color, nowait=True)
+        br = brightness / 1000
+        hex = BulbDevice.rgb_to_hexvalue(*rgb_color, hexformat="hsv16")
+        h, s, _ = BulbDevice.hexvalue_to_hsv(hex, "hsv16")
+        value = BulbDevice.hsv_to_hexvalue(h, s, br, "hsv16")
+
+        for i in range(len(self.__lights)):
+            # br = brightness / light.dpset["value_max"]
+            # value = BulbDevice.hsv_to_hexvalue(h, s, br, "hsv16")
+
+            self.__lights[i].set_multiple_values(
+                {
+                    "21": "colour",
+                    "24": value,
+                },
+                nowait=True,
+            )
 
     def __recover_light_state(self) -> None:
-        for light in self.__lights:
-            status = self.__initial_light_states[light.id]
+        for i in range(len(self.__lights)):
+            light = self.__lights[i]
 
-            for key, value in status.items():
-                light.set_value(index=key, value=value, nowait=False)
+            if i == len(self.__lights) - 1:
+                nowait = False
+            else:
+                nowait = True
+
+            data = self.__initial_light_states[light.id]
+            light.set_multiple_values(data, nowait=nowait)
 
         print("Initial State Restored")
 
     def __push_states(self) -> None:
         while True:
             try:
-                br, cl = self.__process_queue.get(timeout=5)
+                br, cl = self.__process_queue.get(timeout=3)
                 print(f"Br: {br}, R: {cl[0]}, G: {cl[1]}, B: {cl[2]}")
 
                 self.__send_light_state(br, cl)
@@ -145,6 +159,7 @@ class LocalTuyaProcess(multiprocessing.Process):
 
     def kill(self) -> None:
         self.__recover_light_state()
+        sleep(0.25)
         self.__close_connection()
 
         print("Tuya Local Process Killed")
