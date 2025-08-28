2a5c09d56ed73d95ba4991148b221c2b1a88949e
IniyanKanmani <kiniyan002@gmail.com>
2025-08-28 19:29:41 +0530

    feat: implemeted initial energy-based audio processing

--- audio_input_stream_manager.py
+++ audio_input_stream_manager.py
@@ -110,44 +110,52 @@ class AudioInputStreamManager:
         window = np.hanning(frames)[:, None]
         magnitude = np.abs(np.fft.rfft(indata * window, axis=0))
 
-        # print(np.min(magnitude), np.average(magnitude), np.max(magnitude))
-
-        br = (np.max(magnitude) / self.__max_possible_amp) * 255
-        br = np.clip(br, 0, 255)
-
         low_bands = magnitude[self.__bands["low"][0] : self.__bands["low"][1]]
         mid_bands = magnitude[self.__bands["mid"][0] : self.__bands["mid"][1]]
         high_bands = magnitude[self.__bands["high"][0] : self.__bands["high"][1]]
 
-        r = (np.max(low_bands) / self.__max_possible_amp) * 255
+        # Calculate Red
+        low_log = np.log(low_bands + 1)
+        low_min_log = np.min(low_log)
+        low_max_log = np.max(low_log)
+
+        r = np.sqrt(np.average(np.power(low_bands, 2)))
+        r = np.log(r + 1)
+        r = ((r - low_min_log) / (low_max_log - low_min_log)) * 255
         r = np.clip(r, 0, 255)
 
-        # low_band_avg = np.average(low_bands)
-        # low_band_max = np.max(low_bands)
-        # r = int(
-        #     (low_band_max if low_band_max / 2 > low_band_avg else low_band_avg) * 255
-        # )
+        # Calculate Green
+        mid_log = np.log(mid_bands + 1)
+        mid_min_log = np.min(mid_log)
+        mid_max_log = np.max(mid_log)
 
-        g = (np.max(mid_bands) / self.__max_possible_amp) * 255
-        g = np.clip(r, 0, 255)
+        g = np.sqrt(np.average(np.power(mid_bands, 2)))
+        g = np.log(g + 1)
+        g = ((g - mid_min_log) / (mid_max_log - mid_min_log)) * 255
+        g = np.clip(g, 0, 255)
 
-        # mid_band_avg = np.average(mid_bands)
-        # mid_band_max = np.max(mid_bands)
-        # g = int(
-        #     (mid_band_max if mid_band_max / 2 > mid_band_avg else mid_band_avg) * 255
-        # )
+        # Calculate Blue
+        high_log = np.log(high_bands + 1)
+        high_min_log = np.min(high_log)
+        high_max_log = np.max(high_log)
 
-        b = (np.max(high_bands) / self.__max_possible_amp) * 255
-        b = np.clip(r, 0, 255)
+        b = np.sqrt(np.average(np.power(high_bands, 2)))
+        b = np.log(b + 1)
+        b = ((b - high_min_log) / (high_max_log - high_min_log)) * 255
+        b = np.clip(b, 0, 255)
+
+        # Calculate Brightness
+        br_mag = magnitude[self.__bands["low"][0] : self.__bands["high"][1]]
+        mag_log = np.log(br_mag + 1)
+        mag_min_log = np.min(mag_log)
+        mag_max_log = np.max(mag_log)
+
+        br = np.sqrt(np.average(np.power(br_mag, 2)))
+        br = np.log(br + 1)
+        br = ((br - mag_min_log) / (mag_max_log - mag_min_log)) * 255
+        br = np.clip(br, 0, 255)
 
         # print(br, r, g, b)
-
-        # high_band_avg = np.average(high_bands)
-        # high_band_max = np.max(high_bands)
-        # b = int(
-        #     (high_band_max if high_band_max / 2 > high_band_avg else high_band_avg)
-        #     * 255
-        # )
 
         if len(self.__data) < self.__samples_to_average - 1:
             self.__data.append([br, r, g, b])
